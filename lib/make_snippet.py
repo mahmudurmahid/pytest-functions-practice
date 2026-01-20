ba385c4ce078cea79d2f87eb64446e46c3d7a6be
mahmudur mahid <m.mahid@icloud.com>
2026-01-20 15:58:37 +0000

    added comment above make_snippet function to show what it does

--- lib/make_snippet.py
+++ lib/make_snippet.py
@@ -1,2 +1,6 @@
+"""
+A function called make_snippet that takes a string as an argument and 
+returns the first five words and then a '...' if there are more than that
+"""
 def make_snippet():
     pass
