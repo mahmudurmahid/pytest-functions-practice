b3e00268da3d940d0cd03a2eaeb8c49c2a18c2fc
mahmudur mahid <m.mahid@icloud.com>
2026-01-20 16:11:36 +0000

    added test to find if method returns a string value

--- tests/test_make_snippet.py
+++ tests/test_make_snippet.py
@@ -1,1 +1,9 @@
 from lib.make_snippet import *
+
+"""
+the method takes a string as an argument,
+returns a string
+"""
+def test_make_snippet_returns_string_value():
+    result = make_snippet("this method does return a string")
+    assert result == "this method does return a string"
