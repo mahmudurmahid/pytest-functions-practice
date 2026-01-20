2045c4753d5538d2042fc37183794107e8aac06e
mahmudur mahid <m.mahid@icloud.com>
2026-01-20 19:04:07 +0000

    added test to return only first five words from a string

--- tests/test_make_snippet.py
+++ tests/test_make_snippet.py
@@ -5,5 +5,12 @@ the method takes a string as an argument,
 returns a string
 """
 def test_make_snippet_returns_string_value():
-    result = make_snippet("this method does return a string")
-    assert result == "this method does return a string"
+    result = make_snippet("this method returns a string")
+    assert result == "this method returns a string"
+
+"""
+the method only returns the first 5 words of a string argument
+"""
+def test_make_snippet_returns_five_words_only():
+    result = make_snippet("this method return a string even if longer than five letters")
+    assert result == "this method returns a string"
